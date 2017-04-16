4c9f797afc17b8618cbe37640c54c7d3e41f5f0e
Artur Chmaro <artur.chmaro@consileon.pl>
2017-04-16 17:20:49 +0200

    Call API from login form component.

--- client/app/bundles/WebChatter/components/LoginForm.jsx
+++ client/app/bundles/WebChatter/components/LoginForm.jsx
@@ -3,23 +3,41 @@ import { Form, Icon, Input, Button } from 'antd';
 const FormItem = Form.Item;
 
 export default class LoginForm extends React.Component {
-  constructor(props, _railsContext) {
+
+  constructor(props, _railsContext) {   
     super(props);
+   this.state = {
+      email: '',
+      password: ''
+    };    
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleSubmit(e) {
-    e.preventDefault()
-    this.props.loginUser({fullName: 'Bill Gates', email: 'chuj'})
+    e.preventDefault();
+    this.props.authenticateUser(this.state.email, this.state.password);
   }
+
+  handleInputChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }  
 
   render() {
     return (
       <Form onSubmit={(e) => this.handleSubmit(e)} className="login-form">
         <FormItem>
-          <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+          <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} 
+                 placeholder="Email" name='email' value={this.state.email} onChange={this.handleInputChange} />
         </FormItem>
         <FormItem>
-          <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+          <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} 
+                 type="Password" placeholder="Password" name='password' value={this.state.password} onChange={this.handleInputChange} />
         </FormItem>
         <FormItem>
           <Button type="primary" htmlType="submit" className="login-form-button">
